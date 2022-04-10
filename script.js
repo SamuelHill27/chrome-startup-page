9e668f30b327a1a7d72a18cd07cc682ac314db60
SamuelHill27 <johnnyswiftz27@gmail.com>
2022-04-11 00:34:17 +0100

    almost finished left section widgets

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-//weather api
+function getDate() {
+  return new Date();
+}
 
-function weatherApi() {
-  const weatherApiKey = "7464d8f56ed30f0467bc4d7331befa80";
-  const lat = 51.276560;
-  const lon = -0.842150;
-  const weatherFeed = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exlude=daily&appid=" + weatherApiKey;
+function capitalizeWeatherDesc(string) {
+  let flag = false;
+  let firstWord;
+  let secondWord;
 
-  fetch(weatherFeed)
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) == " ") {
+      flag = true;
+      firstWord = string.substring(0, i);
+      secondWord = string.substring(i+1, string.length);
+    }
+  }
+
+  if (flag) {
+    return firstWord.charAt(0).toUpperCase() + firstWord.slice(1) + " " + secondWord.charAt(0).toUpperCase() + secondWord.slice(1);
+  } else {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+}
+
+//news api
+
+newsApi();
+
+function newsApi() {
+  const newsApiKey = "pub_63646ddb98584a445eb1e2bdf0d0c83d6021";
+  const newsFeed = "https://newsdata.io/api/1/news?apikey=" + newsApiKey + "&domain=bbc&category=top";
+
+  fetch(newsFeed)
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      const currentTemp = data['current']['temp'] - 273.15;
-      const currentFeelLike = data['current']['feels_like'] - 273.15;
-      const sunrise = data['current']['sunrise'];
-      const sunset = data['current']['sunset'];
-
+      addNewsStory(data['results']['0']);
+      addNewsStory(data['results']['1']);
+      addNewsStory(data['results']['2']);
+      addNewsStory(data['results']['3']);
+      addNewsStory(data['results']['4']);
     });
+}
 
-  const lastUpdated = date.getHours() + ":" + date.getMinutes();
-  document.getElementById('lastUpdated').innerHTML = lastUpdated;
+function addNewsStory(news) {
+  let tagElements = ["h1", "h2"];
+  let newsElements = ['title', 'description'];
+
+  for (let i = 0; i < 2; i++) {
+    let tag = document.createElement(tagElements[i]);
+    let text = document.createTextNode(news[newsElements[i]]);
+    tag.appendChild(text);
+    let element = document.getElementById("news-container");
+    element.appendChild(tag);
+  }
 }
 
 //current time
@@ -32,21 +66,19 @@ function correctTime(time) {
   return time;
 }
 
-function updateCurrentTime() {
-  let date = new Date();
-
-  let hour = date.getHours();
+function correctHour(hour) {
   if (hour > 12) {
-    hour -= 12;
+    return (hour -= 12);
   }
-  let minute = date.getMinutes();
-  let second = date.getSeconds();
+  return hour;
+}
 
-  let correctedHour = correctTime(hour);
-  let correctedMinute = correctTime(minute);
-  let correctedSecond = correctTime(second);
+function updateCurrentTime() {
+  let hour = getDate().getHours();
+  let minute = getDate().getMinutes();
+  let second = getDate().getSeconds();
 
-  let currentTime = correctedHour + ":" + correctedMinute + ":" + correctedSecond;
+  let currentTime = correctTime(correctHour(hour)) + ":" + correctTime(minute) + ":" + correctTime(second);
   document.getElementById('time').innerHTML = currentTime;
 
   setTimeout(updateCurrentTime, 1000);
@@ -74,4 +106,54 @@ function getTimeZone() { //called when enter button is pressed
   if (timeZone != "") {
     timeApi(timeZone);
   }
+}
+
+//weather api
+
+weatherApi();
+
+function weatherApi() {
+  const weatherApiKey = "7464d8f56ed30f0467bc4d7331befa80";
+  const lat = 51.276560;
+  const lon = -0.842150;
+  const weatherFeed = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exlude=daily&appid=" + weatherApiKey;
+
+  fetch(weatherFeed)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setCurrentWeather(data['current']);
+      setHourlyWeather(data['hourly']);
+      setDailyWeather(data['daily']);
+    });
+}
+
+function setCurrentWeather(data) {
+  document.getElementById('weather-desc').innerHTML = capitalizeWeatherDesc(data['weather']['0']['description']);
+  document.getElementById('wind-speed').innerHTML = "Wind: " + data['wind_speed'] + "ms⁻¹";
+  document.getElementById('temperature').innerHTML = Math.round(data['temp'] - 273.15) + "°C";
+  document.getElementById('feels-like').innerHTML = "Feels Like: " + Math.round(data['feels_like'] - 273.15) + "°C";
+
+  let sunriseSeconds = data['sunrise'];
+  let sunriseDate = new Date(sunriseSeconds*1000);
+  let sunrise = correctTime(correctHour(sunriseDate.getHours())) + ":" + correctTime(correctHour(sunriseDate.getMinutes()));
+  let sunsetSeconds = data['sunset'];
+  let sunsetDate = new Date(sunsetSeconds*1000);
+  let sunset = correctTime(correctHour(sunsetDate.getHours())) + ":" + correctTime(correctHour(sunsetDate.getMinutes()));
+  document.getElementById('sunrise').innerHTML = "Sunrise: " + sunrise;
+  document.getElementById('sunset').innerHTML = "Sunset: " + sunset;
+
+  document.getElementById('current-weather-icon').src="http://openweathermap.org/img/wn/" + data['weather']['0']['icon'] + "@2x.png";
+}
+
+function setHourlyWeather(data) {
+
+}
+
+function setDailyWeather(data) {
+
+}
+
+function setWeatherIcons() {
+
 }
